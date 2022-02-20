e11f31e2e78ae586469759b5278b406c16c3735d
Rafael Laube <laube.developer@gmail.com>
2022-02-20 19:04:42 -0300

    Alteração das variáveis de ambiente.

--- src/web/src/utils/database.js
+++ src/web/src/utils/database.js
@@ -1,11 +1,7 @@
 import { MongoClient } from "mongodb";
 
 
-const db_url =
-    "mongodb+srv://"+
-    process.env.DB_USERNAME +
-    ":"+
-    process.env.DB_SENHA +"@cluster0.hns3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const db_url = "mongodb+srv://admin:"+ process.env.DB_PWD +"@cluster0.hns3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const client = new MongoClient(db_url, {
     useNewUrlParser: true,
